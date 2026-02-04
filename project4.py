0c00c5235d78a270e825cb3e4a7fceddaf30cb35
wbaker12345 <willbaker@seattleacademy.org>
2026-02-04 22:20:00 +0000

    hhh

---
+++ project4.py
@@ -0,0 +1,67 @@
+import turtle, time, random
+from utils import *
+
+# Section 1 - setup
+# TODO - set a background using 
+set_background("drakeback")
+t1 =create_sprite("talk",-350, 150)
+
+# TODO - create at least two variables and set their starting value. ex: cookies = 0
+fans = 0
+cost = 10
+album = 0
+song = 0
+
+
+# Section 2 - controls
+# TODO - define an action. ex: def my_control()
+def make_fans () :
+    global fans
+    fans += 1
+    x = random.randint (200,300)
+    y = random.randint (-250,200)
+    create_sprite("fans", x, y)
+# TODO - choose a key to do the action. ex: window.onkeypress(my_control, "space")
+window.onkeypress(make_fans, "space")
+
+
+
+# TODO - make a second control
+def make_album() :
+    global album, fans, cost
+    if fans >= cost:
+        fans = fans - cost
+        cost = cost +5
+        album+=1
+        x = -200 + 15 *fans
+        y = -150
+        create_sprite("album", x, y)
+# TODO - choose a key to do the action. ex: window.onkeypress(my_control, "space")
+window.onkeypress(make_album, "c")
+
+
+
+def make_song() :
+    global make_song, fans, cost
+    if fans >= cost:
+        fans = fans - cost
+        cost = cost +5
+        make_song +=1
+        x = -200 + 15 *fans
+        y = -150
+        create_sprite("song", x, y)
+# TODO - choose a key to do the action. ex: window.onkeypress(my_control, "space")
+window.onkeypress(make_song, "h")
+
+
+
+
+# Section 3 - game loop
+window.listen()
+for i in range(1000000000):
+    t1.clear()
+    t1.write(f"fans: {fans}\ncost: {cost}\nalbum: {album}", font=("Arial",30,"normal"))
+    t1.color("pink")
+    # TODO - put any repeating actions here
+    time.sleep(0.01)
+    window.update() 
